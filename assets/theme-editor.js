ba3e26ae343318e19d9f97c35c7e53317759b7bd
coder sifat <sifatcreator999@gmail.com>
2023-07-29 01:35:38 +0600

    our theme section collection list featured collection

---
+++ assets/theme-editor.js
@@ -0,0 +1,33 @@
+document.addEventListener('shopify:block:select', function(event) {
+  const blockSelectedIsSlide = event.target.classList.contains('slideshow__slide');
+  if (!blockSelectedIsSlide) return;
+
+  const parentSlideshowComponent = event.target.closest('slideshow-component');
+  parentSlideshowComponent.pause();
+
+  setTimeout(function() {
+    parentSlideshowComponent.slider.scrollTo({
+      left: event.target.offsetLeft
+    });
+  }, 200);
+});
+
+document.addEventListener('shopify:block:deselect', function(event) {
+  const blockDeselectedIsSlide = event.target.classList.contains('slideshow__slide');
+  if (!blockDeselectedIsSlide) return;
+  const parentSlideshowComponent = event.target.closest('slideshow-component');
+  if (parentSlideshowComponent.autoplayButtonIsSetToPlay) parentSlideshowComponent.play();
+});
+
+
+
+
+$( document ).ready(function() {
+   $('.text-slider').slick({
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+  });
+});
+
